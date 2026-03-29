e8a0c741d32cd07b930f2349f4cad2d3f5a21740
Cami-09 <camicarmona1212@gmail.com>
2026-03-29 00:25:49 -0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDLucWQMOgeUx3e5H8AbFCml7AosZ2eVHw",
+  authDomain: "sistema-de-tickets-ti-b12f5.firebaseapp.com",
+  projectId: "sistema-de-tickets-ti-b12f5",
+  storageBucket: "sistema-de-tickets-ti-b12f5.firebasestorage.app",
+  messagingSenderId: "356054718850",
+  appId: "1:356054718850:web:3a439d4ffbd4f017a7179b",
+  measurementId: "G-MKESSZYG8P"
+};
